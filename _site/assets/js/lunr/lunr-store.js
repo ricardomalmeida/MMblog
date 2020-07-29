07cae5db7b1ccaa55f640df2ac741000d75e3622
Ricardo Almeida <ricardo.almeida.mail@gmail.com>
2020-07-29 16:39:47 +0100

    post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "Primeiro post!",
+        "excerpt":"Primeiro post do blog!  Finalmente eis aqui o verdadeiro post. Está criado o meio, a plataforma. Posso “postar”.  ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "Segundo Post",
+        "excerpt":"Segundo post !   segunda “postadela”.  ","categories": ["jekyll","linux","work"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/linux/work/segundo-post/",
+        "teaser": null
+      }]
